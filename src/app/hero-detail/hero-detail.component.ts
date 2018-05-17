1cca3a3cf115c9267b0ad537caa02537690bb7c4
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2018-05-17 20:54:04 +0300

    finish with routing to hero-detail

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -35,4 +35,7 @@ export class HeroDetailComponent implements OnInit {
     this.heroService.getHero(id)
                     .subscribe(hero => this.hero = hero);
   }
+  goBack(): void {
+    this.location.back();
+  }
 }
