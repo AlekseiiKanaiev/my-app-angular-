55026b63d49e53ac5eb7e24d398946c70baf0298
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2018-05-17 17:58:03 +0300

    configure RouterModule and add RouterOutlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import {HeroesComponent} from './heroes/heroes.component';
 
+/**Добавляем роуты для перехода
+ * На пример: для адреса localhost:4200/heroes будет "преход" на
+ * компонент HeroesComponent*/
 const routes: Routes = [
   {path: 'heroes', component: HeroesComponent}
 ];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    /**Добавляем модуль RouterModule и настраиваем его на роуты
+     *
+     *The method is called forRoot() because you configure the
+     router at the application's root level.
+     The forRoot() method supplies the service providers and
+     directives needed for routing, and performs the initial
+     navigation based on the current browser URL.*/
+    RouterModule.forRoot(routes)
   ],
   declarations: [],
   /**делаем доступным директивы RouterModule в AppModule*/
