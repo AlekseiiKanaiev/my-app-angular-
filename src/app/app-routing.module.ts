804635e46cfbc86b8e4ed0e9c0e3a3cca6426c1c
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2018-05-17 18:27:35 +0300

    add dashboard component and routing to it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import {HeroesComponent} from './heroes/heroes.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 /**Добавляем роуты для перехода
  * На пример: для адреса localhost:4200/heroes будет "преход" на
  * компонент HeroesComponent*/
 const routes: Routes = [
-  {path: 'heroes', component: HeroesComponent}
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
